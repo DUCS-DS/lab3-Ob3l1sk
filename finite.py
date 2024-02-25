1ce8a60d196912b6c43d5788adc7938db08012f5
Dalton "Ray" Hammon <dhammon004@drury.edu>
2024-02-24 19:15:04 -0600

    Implemented a length(lst) function, a llprint(lst) function and created temp code that can be ran with lines 24-40. This should meet the criteria of exercise 1-5

--- finite.py
+++ finite.py
@@ -3,18 +3,38 @@ from llist import LList, Node, append
 
 def length(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
+    count = 0
+    node = lst.head
+    while node is not None:
+        count = count + 1
+        node = node.next  
+    return(count)    
 
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
-
+    node = lst.head
+    while node is not None:
+        print(node.val)
+        node = node.next
+        
 
 if __name__ == "__main__":
 
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+    llist = LList()
+    append(llist, Node(1))
+    append(llist, Node(2))
+    append(llist, Node(4))
+    append(llist, Node(8))
+    append(llist, Node(16))
+    append(llist, Node(32))
+    append(llist, Node(64))
+    append(llist, Node(128))
+    append(llist, Node(256))
+    append(llist, Node(512))
+    
+    length(llist)
+    print("The Length of the llist is:" ,length(llist))
+    llprint(llist)
+    from genfinite import lst
+    print("The Length of lst is: ",length(lst))
